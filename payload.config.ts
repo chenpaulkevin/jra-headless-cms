e19ac9a6de157228be949950baa694bcac83776f
chenpaulkevin <47184835+chenpaulkevin@users.noreply.github.comchenpaulkevin@gmail.com>
2024-06-01 01:16:49 +0800

    ✨ feat(Lexical Editor): Embed Link
    - Add Link Embed to lexical editor

--- payload.config.ts
+++ payload.config.ts
@@ -38,21 +38,14 @@ export default buildConfig({
       ...defaultFeatures,
       FixedToolbarFeature(),
       LinkFeature({
-        // Example showing how to customize the built-in fields
-        // of the Link feature
-        fields: [
-          {
-            name: 'rel',
-            label: 'Rel Attribute',
-            type: 'select',
-            hasMany: true,
-            options: ['noopener', 'noreferrer', 'nofollow'],
-            admin: {
-              description:
-                'The rel attribute defines the relationship between a linked resource and the current document. This is a custom link field.',
-            },
-          },
-        ],
+        enabledCollections: [],
+        fields: ({ defaultFields }) =>
+          defaultFields.map((field) => {
+            if ('name' in field && field.name === 'url' && field.type === 'text') {
+              delete field.validate
+            }
+            return field
+          }),
       }),
     ],
   }),
