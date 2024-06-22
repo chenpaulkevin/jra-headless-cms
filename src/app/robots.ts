cfc80de86131598281687fba0c2ee2d7fc269fe5
chenpaulkevin <47184835+chenpaulkevin@users.noreply.github.comchenpaulkevin@gmail.com>
2024-06-22 20:03:41 +0800

    Configure SEO rules and dynamic sitemap URL for robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: [],
+      disallow: ['/api', '/admin'],
     },
     sitemap: `${seoUrl}/sitemap.xml`,
   }
